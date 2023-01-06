a057da89f8857e32c77f6c9736b9eb81f701486b
HelenHMac <120410707+HelenHMac@users.noreply.github.com>
2023-01-06 00:19:47 -0500

    Added HTML structure, css, react, axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,86 @@
-
 import './App.css';
-import Weather from "./weather"
 
 
-function App() {
+export default function App() {
+  let weather = {
+    city: "New York",
+    date: "Sunday 11pm",
+    description: "rainy",
+    humidity: "40",
+    wind: "6",
+    temperature: "13",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-      <h1><Weather city="New York"/></h1>
-      </header>
+      <div>
+        <h1 className="app">
+          <div>Vanilla Weather App</div>
+        </h1>
+        <div className="weather-app">
+        <form action="" id="search-form" className="mb-3">
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                class="form-control"
+                id="city-input"
+                autocomplete="off"
+              />
+            </div>
+            <div className="col-4">
+              <input type="submit" value="Search" className="button" />
+            </div>
+          </div>
+        </form>
+        
+
+        <table>
+          <tr className="row2">
+            <td className="morespace">
+              <ul className="listnone">
+                <li className="city">{weather.city}</li>
+                <li id="date" className="inline kursiv">
+                  {weather.date}, {weather.description}
+                </li>
+                <li className="kursiv">
+                  Humidity: {weather.humidity}%, Wind: {weather.wind} km/h
+                </li>
+              </ul>
+            </td>
+            <td>
+              <span>
+                <strong className="temperaturemain">
+                  {weather.temperature}
+                </strong>
+                <sup className="degree">
+                  <a href="/" className="units active">
+                    °C
+                  </a>
+                  |
+                  <a href="/" className="units">
+                    F
+                  </a>
+                </sup>
+              </span>
+            </td>
+            <td>
+              <img src={weather.imgUrl} alt="" className="image1" />
+            </td>
+          </tr>
+        </table>
+        </div>
+        <footer>
+          <a
+            href="https://github.com/HelenHMac/Vanilla-weather-app-advanced"
+            className="footer-app"
+          >
+            Open source code by Olena Honcharova
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
